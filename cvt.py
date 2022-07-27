2f6a92d17ff25bd033f2c9c31cbe032d12773da4
ravi <rnukala@goshippo.com>
2022-07-27 11:22:17 -0700

    More datetime formats (Germany) debugs setup as a method (NEED flag/ENV soon)

--- cvt.py
+++ cvt.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+
 import pendulum as pdl
 import sys
+
 
 # pendulum.parse(str)
 # pendulum.from_format('Saturday July 23 11:05pm', 'dddd MMMM DD hh:mma')
@@ -22,7 +24,13 @@ class Cvt:
     fmts = [
         '',
         'dddd MMMM DD hh:mma z',  # Saturday July 23 11:00pm AEST
-        'MMMM DD hh:mma z'  # July 23 11:00pm AEST
+        'MMMM DD hh:mma z',  # July 23 11:00pm AEST
+        'ddd DD MMMM hh:mma z', # Sun 17 July 5:00am AEST
+        'dddd, DD.MM.YYYY ha z', # Wednesday, 27.07.2022 5am CEST
+        'hh:mma z', # 7:39PM UTC
+        'hh:ma z',  # 7:39PM UTC
+        'h:mma z',  # 7:39PM UTC
+        'h:ma z',  # 7:39PM UTC
     ]
 
     def __init__(self, time_str):
@@ -30,8 +38,9 @@ class Cvt:
         self.dt = None
         self._normalize()
         self.debug = False
+        self._parse()
 
-    def _dbg(self, string):
+    def dbg(self, string):
         if self.debug:
             print(f" >>dbg: {string}")
         else:
@@ -39,13 +48,30 @@ class Cvt:
 
     def _normalize(self) :
         # if 'AEST' in dtstr.capitalize():
-        dt_str = self.orig_str.replace('AEST', 'Australia/Sydney').replace('AEDT', 'Australia/Sydney')
+        # trim spaces by strip
+        # translate to remove punctuation
+        #   translate(str.maketrans('', '', string.punctuation)) REMOVES ':' not acceptable \
+        dt_str = self.orig_str \
+            .replace(',', '').replace('.', '').replace('#', '') \
+            .strip()\
+            .replace('AEST', 'Australia/Sydney').replace('AEDT', 'Australia/Sydney')\
+            .replace("CEST", 'Europe/Berlin').replace("CET", 'Europe/Berlin')
         # print(dtstr)
         self.time_str = dt_str
 
+    @staticmethod
+    def is_datetime_string(dtstr):
+        if not dtstr and len(dtstr) <= 0:
+            return False
+        return True
+
     def _parse(self):
+        if not self.is_datetime_string(self.time_str):
+            return
+        if self.dt:
+            return
         for fmt in self.fmts:
-            #print(f"str={self.time_str}, dt={self.dt} and format={fmt}")
+            self.dbg(f"str={self.time_str}, dt={self.dt} and format={fmt}")
             try:
                 if not self.dt:
                     if len(fmt) == 0:
@@ -56,13 +82,13 @@ class Cvt:
                 else:
                     break
             except ValueError as ve:
-                #print(f"  [{fmt}] error: {repr(ve)}")
+                self.dbg(f"  [{fmt}] error: {repr(ve)}")
                 pass
 
     def toPdt(self):
         self._parse()
         if not self.dt:
-            return ""
+            return f"NOTHING for [{self.time_str}]"
         ca_tz = pdl.timezone('America/Los_Angeles')
         ca_dt = self.dt.in_tz(ca_tz)
         return ca_dt.strftime('%B %d %I:%M %p %Z')
@@ -72,12 +98,12 @@ class Cvt:
 if __name__ == "__main__":
     #dtstr = "2022-07-13T12:33:14.859Z"
     dtstr = "Saturday July 23 11:03pm AEDT "
-    # dt = pdl.from_format(dtstr, 'dddd MMMM DD hh:mma z')
-    # print(f"dt={dt}")
+    dtstr = "7:39PM UTC"
+    #dt = pdl.from_format(dtstr, 'hh:mma z')
 
     if len(sys.argv) > 1:
         dtstr = sys.argv[1]
 
     cvt = Cvt(dtstr)
-    #print(f"[{dtstr}] becomes\n{cvt.toPdt()}")
+    cvt.dbg(f"[{dtstr}] becomes\n{cvt.toPdt()}")
     print(f"{cvt.toPdt()}")
