d7dde8428f30655d6eb19c3aacdeb46217ec496b
ravi <nukala@gmail.com>
2024-06-17 13:05:58 -0700

    md5 - command line args parsed

--- md5.py
+++ md5.py
@@ -2,7 +2,9 @@
 # coding: utf-8
 
 ###############################################################################
-# WIP - due to mmap 
+# WIP - due to mmap
+#     - yucky way in which files are passed.
+#        illegal arguments like -xxx are now considered files and then no help
 ###############################################################################
 
 from argparse import ArgumentParser
@@ -17,7 +19,7 @@ class md5:
   def __init__(self):
     self.BLOCK_SZ = 8192
 
-  def process(self, fname):
+  def process_block(self, fname):
     hasher = hashlib.md5()
     with open(fname, "rb") as ff:
       hunk = ff.read(self.BLOCK_SZ)
@@ -25,11 +27,10 @@ class md5:
         hasher.update(hunk)
         hunk = ff.read(self.BLOCK_SZ)
 	
-      self.theHash = hasher.hexdigest()
-      self.fname = fname
+    return hasher.hexdigest()
 
   # TODO: work in progress, there is a permission error while mapping
-  def processMmap(self, fname):
+  def process_mmap(self, fname):
     hasher = hashlib.md5()
     with open(fname, "rb") as ff:
       mm = mmap.mmap(ff.fileno(), 0)
@@ -38,55 +39,56 @@ class md5:
         hasher.update(hunk)
         hunk = mm.read(self.BLOCK_SZ)
 	
-      self.theHash = hasher.hexdigest()
-      self.fname = fname
+    return hasher.hexdigest()
 
-  def processInline(self, fname):
+  def process_inline(self, fname):
     with open(fname, "rb") as ff:
       digest = hashlib.file_digest(ff, "md5")
     
-    self.theHash = digest.hexdigest()
-    self.fname = fname
-
+    return digest.hexdigest()
+  
   def parse_args(self, args=None):
     parser = ArgumentParser(prog = 'md5',
-                            description="To generate md5 sum of specified files.")
+                            description="To generate md5 sum of specified files in a platform agnostic way.")
     parser.add_argument('-v', '--verbose', action='store_true', default = False, dest = "verbose", 
                         help="Enable verbosity")
     parser.add_argument('-s', '--short', action='store_true', default = False, dest = "short",
             help="Short output, no filename")
-    parser.add_argument('--memmap', "--memory_map", action='store_true', default = False, 
+    parser.add_argument('--mmap', "--memory_map", action='store_true', default = False, 
                         dest = "use_mmap", help="Use memory mapped files. DOES NOT WORK")
-    parser.add_argument('--hashlib', "--use_hashlib", action='store_true', default = False, 
-                        dest = "use_hashlib", help="Use slower hashlib based operations")
-    parser.add_argument("-f", "--files", nargs='*', dest="files", help="names of files to md5 summed")
+    parser.add_argument('--block', "--use_block", action='store_true', default = False, 
+                        dest = "use_block", help="Use slower hashlib based operations")
 
-    self.parsed = parser.parse_args(args)
+    self.parsed, self.unknown_args = parser.parse_known_args(args)
 
-    file_names=[]
-    for f in self.parsed.files:
-      file_names += glob(f)
-    self.file_names = file_names
-
-
-  def __str__(self):
-    return f"{self.theHash}  {self.fname}"
+    if self.parsed.short and len(self.unknown_args) > 0:
+      raise Exception(f"Short={self.parsed.short} and unknown_args{self.unknown_args}"
+                      ", len={len(self.unknown_args)} are not compatible")
 
 
 ###### end of md5 class
 
 if __name__ == "__main__":
+  # fname = sys.argv[1]
   msum = md5()
-  # msum.parse_args()
+
+  msum.parse_args()
 
   # preparation to help measure time spent
-  # if msum.parsed.use_hashlib:
-  #   msum.process()
-  # elif msum.parsed.use_mmap:
-  #   msum.processMmap()
-  # else: 
-  #   msum.processInline()
+  for fname in msum.unknown_args:
+    the_hash = 'unknown'
+    try:
+      if msum.parsed.use_block:
+        the_hash = msum.process_block(fname)
+      elif msum.parsed.use_mmap:
+        the_hash = msum.process_mmap(fname)
+      else:
+        the_hash = msum.process_inline(fname) 
 
-  for i in range(1):
-    msum.processInline(sys.argv[1])
-    print(f"{msum}")
+      if (msum.parsed.short):
+        print(f"{the_hash}")
+      else:
+        print(f"{the_hash}\t{fname}")
+    except (OSError, PermissionError) as e:
+      print(f"{e}")
+      pass;
