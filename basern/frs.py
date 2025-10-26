754ed246aeeb44d0d59c68d70a80d971edd98639
ravi <nukala@gmail.com>
2025-10-25 17:17:25 -0700

    frs: calculated_threshold, many irs/ftb array elements fixed,      accurate ftb 2025 values

--- basern/frs.py
+++ basern/frs.py
@@ -10,48 +10,76 @@ from typing import List
 # To calculate the total tax based on values seen in tables that show pct + min
 #
 # Missing 401k contributions
+# develop a Brak class instead of an array
 #
+# see also https://taxfoundation.org/data/all/federal/tax-calculator-obbba/
 #############################################
 
+INFINITY = 999_999_999
 
 class FtbIrsUtils:
   @staticmethod
-  def find_row(inc, ary, parsed):
+  def find_row(inc, ary, desc, parsed):
     for i in ary:
       min_amt = i[0]
       max_amt = i[1]
 
       if min_amt <= inc <= max_amt:
-        if parsed.verbose > 1:
-          print(f"   For ${inc}, bracket={i}")
+        if parsed.verbose > 0:
+          print(f"   {desc}=${inc}, bracket={i}, min={min_amt}, max={max_amt}")
         return i
 
     return None
 
   @staticmethod
-  def calc_payment(inc, deduct, ary, parsed):
+  def calc_payment(inc, deduct, ary, parsed, desc='IRS'):
     orig_inc = inc
     if parsed.verbose > 2:
       print(f"   inc={inc} will be reduced by {deduct}")
     inc -= deduct
     inc = max(inc, 0)
 
-    r = FtbIrsUtils.find_row(inc, ary, parsed)
+    r = FtbIrsUtils.find_row(inc, ary, desc, parsed)
 
     if r is None:
-      if parsed.verbose > 1:
-        print(f"   nothing found for inc={inc}")
+      print(f"  ERR> nothing found for inc={inc}")
       return -1
 
     over = (inc - r[0])
-    threshold = r[2]
+    threshold = r[4]
     fraction = r[3]/100.0
     payment = (fraction*over) + threshold
     if parsed.verbose > 2:
-      print(f"   inc={inc}, over={over}, threshold={threshold}, fraction={fraction:.4f}"
+      print(f"   {desc}-inc={inc}, over={over}, threshold={threshold}, fraction={fraction:.4f}"
             f", payment={payment:.2f}, raw_income={orig_inc}\n")
     return payment
 
+  @staticmethod
+  def process_ary(ary, verbose):
+    s_ary = sorted(ary, key=lambda x: x[0])
+    total = 0.0
+    for i in range(len(s_ary)):
+      s = s_ary[i]
+      s.append(total)
+      # amount to charge at this percentage!
+      to_charge = s[1] - s[0]
+      curr = to_charge * (s[3]/100.0)
+      if verbose > 5:
+        print(f"  s={s}, curr={curr}, total={total}")
+      total = round(total + curr, 2)
+
+    if verbose > 3:
+      import pprint
+      pretty_str = pprint.pformat(s_ary, indent=2, underscore_numbers=True)
+      print(f"processed = {pretty_str}")
+
+    for s in s_ary:
+      typed_threshold = s[2]
+      calc_threshold = s[4]
+      if typed_threshold != calc_threshold:
+        print(f"  ERR> ({s[0]}/{s[1]}): typed={typed_threshold}, calc={calc_threshold}")
+        return None
+    return s_ary
 
 # _nw due to nerdwallet
 class FtbIrsNW:
@@ -63,61 +91,29 @@ class FtbIrsNW:
 
   # https://www.nerdwallet.com/article/taxes/california-state-tax
   ftb = [
-    [0, 21512, 0, 1],
-    [21513, 50998, 215.12, 2],
-    [50999, 80490, 804.84, 4],
-    [80491, 111732, 1985.52, 6],
-    [111733, 141212, 3859.04, 8],
-    [141213, 721318, 6217.44, 9.3]
+    [0, 21_512, 0, 1],
+    [21_512, 50_998, 215.12, 2],
+    [50_998, 80_490, 804.84, 4],
+    [80_490, 111_732, 1984.52, 6],
+    [111_732, 141_212, 3859.04, 8],
+    [141_212, 721_318, 6217.44, 9.3]
   ]
 
   # https://www.nerdwallet.com/article/taxes/federal-income-tax-brackets#2024-tax-brackets-(taxes-due-april-2025)
   irs = [
-    [0, 23200, 0, 10],
-    [23201, 94300, 2320, 12],
-    [94301, 201050, 10852, 22],
-    [201051, 383900, 34337, 24],
-    [383901, 487450, 78221, 32],
-    [487451, 731200, 111357, 35]
+    [0, 23_200, 0, 10],
+    [23_200, 94_300, 2_320, 12],
+    [94_300, 201_050, 10_852, 22],
+    [201_050, 383_900, 34_337, 24],
+    [383_900, 487_450, 78_221, 32],
+    [487_450, 731_200, 111_357, 35]
   ]
 
   def calc_irs(self, inc, parsed):
-    return FtbIrsUtils.calc_payment(inc - self.exempt_irs, self.irs_std, self.irs, parsed)
+    return FtbIrsUtils.calc_payment(inc - self.exempt_irs, self.irs_std, self.irs, parsed, desc='IRS')
 
   def calc_ftb(self, inc, parsed):
-    return FtbIrsUtils.calc_payment(inc - self.exempt_ftb, self.ftb_std, self.ftb, parsed)
-
-class FtbIrs2025(FtbIrsNW):
-  def __init__(self):
-    super(FtbIrs2025, self)
-    self.year = 2025
-    self.irs_std = 31_500 # Ob3a
-    self.ftb_std = 10_726
-    self.exempt_ftb = 298
-
-    # https://www.nerdwallet.com/article/taxes/federal-income-tax-brackets
-    self.irs = [
-      [0, 23_850, 0, 10],
-      [23_851, 96_950, 2385, 12],
-      [96_951, 206_700, 11_157, 22],
-      [206_701, 394_600, 35_302, 24],
-      [394_601, 501_050, 80_398, 32],
-      [501_051, 751_600, 114_462, 35],
-      [751_601, 99_999_999, 202_154.50, 37]
-    ]
-
-    # https://blog.turbotax.intuit.com/income-tax-by-state/california-105369 && inp && brak_ary
-    self.ftb =  [
-    [0, 21_512, 0.00, 1.0],
-    [21_513, 50_998, 215.12, 2.0],
-    [50_999, 80_490, 804.82, 4.0],
-    [80_491, 111_732, 1_984.46, 6.0],
-    [111_733, 141_212, 3_858.92, 8.0],
-    [141_213, 721_318, 6_217.24, 9.3],
-    [721_319, 865_574, 60_167.01, 10.3],
-    [865_575, 1_442_628, 75_025.27, 11.3],
- ]
-
+    return FtbIrsUtils.calc_payment(inc - self.exempt_ftb, self.ftb_std, self.ftb, parsed, desc='ftb')
 
 class FtbIrs2023(FtbIrsNW):
   def __init__(self):
@@ -129,13 +125,13 @@ class FtbIrs2023(FtbIrsNW):
 
     # https://www.irs.gov/media/166986 Schedule Y-1
     self.irs = [
-      [0, 21_999, 0, 10],
+      [0, 22_000, 0, 10],
       [22_000,	89_450,	2_200.00, 12],
       [89_450, 190_750, 10_294.00, 22],
       [190_750, 364_200, 32_580.00, 24],
       [364_200, 462_500, 74_208.00, 32],
       [462_500, 693_750, 105_664.00, 35],
-      [693_750, 99_000_000, 186_601.50, 37]
+      [693_750, INFINITY, 186_601.50, 37]
     ]
 
     # https://www.ftb.ca.gov/forms/2023/2023-540-tax-rate-schedules.pdf Schedule Y
@@ -148,9 +144,42 @@ class FtbIrs2023(FtbIrsNW):
       [136_700, 698_274, 6_018.80, 9.3],
       [698_274, 837_922,  58_245.18, 10.3],
       [837_922, 1_396_542, 72_628.92, 11.3],
-      [1_396_542, 99_000_000,  135_752.98, 12.3]
+      [1_396_542, INFINITY,  135_752.98, 12.3]
     ]
 
+class FtbIrs2025(FtbIrsNW):
+  def __init__(self):
+    super(FtbIrs2025, self)
+    self.year = 2025
+    self.irs_std = 31_500 # Ob3a
+    self.ftb_std = 10_726
+    self.exempt_ftb = 298
+
+    # https://www.irs.gov/newsroom/irs-releases-tax-inflation-adjustments-for-tax-year-2026-including-amendments-from-the-one-big-beautiful-bill#:~:text=24%25%20for%20incomes%20over%20%24105%2C700,for%20married%20couples%20filing%20jointly
+    self.irs = [
+      [0, 23_850, 0, 10],
+      [23_850, 96_950, 2385, 12],
+      [96_950, 206_700, 11_157, 22],
+      [206_700, 394_600, 35_302, 24],
+      [394_600, 501_050, 80_398, 32],
+      [501_050, 751_600, 114_462, 35],
+      [751_600, INFINITY, 202_154.50, 37]
+    ]
+
+    # https://www.ftb.ca.gov/about-ftb/newsroom/tax-news/index.html
+    self.ftb =  [
+      [0, 22_158, 0.00, 1.0],
+      [22_158, 52_528, 221.58, 2.0],
+      [52_528, 82_904, 828.98, 4],
+      [82_904, 115_084, 2_044.02, 6],
+      [115_084,	145_448, 3_974.82, 8],
+      [145_448, 742_958, 6_403.94, 9.3],
+      [742_958, 891_542, 61_972.37, 10.3],
+      [891_542, 1_485_906, 77_276.52, 11.3],
+      [1_485_906, INFINITY, 144_439.65, 12.3]
+    ]
+
+#### once fully populated, use root parent class
 class FtbIrs2026(FtbIrs2025):
 
   def __init__(self):
@@ -161,13 +190,13 @@ class FtbIrs2026(FtbIrs2025):
 
     # https://www.msn.com/en-us/money/taxes/the-new-irs-tax-brackets-for-2026-are-here-see-where-you-fit-in/ar-AA1Oa7ms
     self.irs = [
-        [0, 24_800, 10, 0],
-        [24_801, 100_800, 2_480, 12],
-        [100_801, 211_400, 11_600, 22],
-        [211_401, 403_550, 35_932, 24],
+        [0, 24_800, 0, 10],
+        [24_800, 100_800, 2_480, 12],
+        [100_800, 211_400, 11_600, 22],
+        [211_400, 403_550, 35_932, 24],
         [403_550, 512_450, 82_048, 32],
         [512_450, 768_700, 116_896, 35],
-        [768_701, 99_999_999, 206_583.50, 37]
+        [768_700, INFINITY, 206_583.50, 37]
     ]
 
 
@@ -236,6 +265,8 @@ class FrsApp:
       if self.parsed.verbose >= 8:
         print(f"  inited 25")
 
+    self.frs.irs = FtbIrsUtils.process_ary(self.frs.irs, self.parsed.verbose)
+    self.frs.ftb = FtbIrsUtils.process_ary(self.frs.ftb, self.parsed.verbose)
     if self.parsed.verbose > 3:
       print(f"  parsed={str(self.parsed)}")
       print(f"  frs={str(self.frs)}, yr={self.frs.year}, std={self.frs.irs_std:,}/{self.frs.ftb_std:,}")
