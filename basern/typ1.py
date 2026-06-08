ce9f66dc680f9f1a8a6807d2fe59e8d1354c75c6
ravi <nukala@gmail.com>
2026-06-08 15:26:14 -0700

    typ1 - single and multi-line help debugging        -vv and --verbose setup, dict instead of dataclass

--- basern/typ1.py
+++ basern/typ1.py
@@ -1,9 +1,44 @@
 import typer
+from typing import Annotated
 
-app = typer.Typer(help="some inline help string", no_args_is_help=True)
+# help="Program help inline string"
+app = typer.Typer(no_args_is_help=True)
+#vbsty = typer.Option("--verbosity", "-v", count=True, default=0)
+# could be a config data class later! 
+state = { }
 
+######
+# For common options among both the commands!
+######
+@app.callback()
+def main(
+    verbosity: Annotated[
+        int,
+        typer.Option("-v", count=True, help="Increase verbosity level use -vvv for 3.")
+    ] = 0,
+    vlevel: Annotated[
+        int,
+	typer.Option("--verbosity", help="Specify a verbosity level")
+    ] = 0,
+):
+    """
+    Main application entry point
+    is this a 
+    multi-line help?
+    
+    Not a multi-line, but this shows up only when help-paramter is removed from Typer ctor
+    """
+    # 2. Assign the parsed value to the shared state
+    state["verbosity"] = verbosity
+    if vlevel > 0:
+      state["verbosity"] = vlevel
+#      state["vlevel"] = vlevel
 
-@app.command()
+@app.command(help="show internal configs future dataclass, now a dictionary")
+def show():
+    print(state)
+
+@app.command(help="say hello the name specified!")
 def hello(name: str):
     print(f"Hello {name}")
 
