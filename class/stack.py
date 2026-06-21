33095fcb47f6a4698d304d87c95f586eda9eefc3
ravi <nukala@gmail.com>
2026-06-21 09:35:36 -0700

    stack - choose sys or stack with different indices stk-tmr - cleanup and attribution

--- class/stack.py
+++ class/stack.py
@@ -1,5 +1,11 @@
 import inspect
+import sys
 
+
+###
+# Why this complexity around stack, sys etc? 
+#   see stk-tmr.sh for timing details!
+##
 class StackDump:
  
   def a(self) -> None:
@@ -13,18 +19,32 @@ class StackDump:
 
   def d(self) -> None:
     func: str=inspect.currentframe().f_code.co_name
-    print(f"In {func}")
-    caller: str = inspect.stack()[1][3]
-    print(f"Caller = {caller}")
-    par: str = inspect.stack()[2][3]
-    print(f"parent = {par}")
-    gpar: str = inspect.stack()[3][3]
-    print(f"grandparent caller = {gpar}")
+    # print(f"In {func}")
 
-    print(f"\n\nSo: {gpar} -> {par} -> {caller} -> {func}")
+    caller: str = inspect.stack()[1][3]
+    # print(f"Caller = {caller}")
+
+    par: str = inspect.stack()[2][3]
+    # print(f"parent = {par}")
+
+    gpar: str = inspect.stack()[3][3]
+    # print(f"grandparent caller = {gpar}")
+
+    print(f"So: {gpar} -> {par} -> {caller} -> {func}")
+    print(f"Depths: {get_func_name(4)} -> {get_func_name(3)} -> {get_func_name(2)} -> {get_func_name(1)}")
+    print(f"Sys: {get_func_name(5, True)} -> {get_func_name(4, True)} -> {get_func_name(3, True)}" 
+      + f" -> {get_func_name(2, True)}")
     # save this comment
     #caller: str = inspect.currentframe().f_back.f_code.co_name
     # inspect.stack() for all traces!
+
+def get_func_name(depth:int = 0, use_sys:bool = False):
+  if use_sys:
+    return get_func_name_with_sys(depth)
+  return inspect.stack()[depth][3]
+  
+def get_func_name_with_sys(depth:int = 0):
+  return sys._getframe(depth).f_code.co_name
 
 if __name__ == "__main__":
     StackDump().a()
--- class/stk-tmr.sh
+++ class/stk-tmr.sh
@@ -3,15 +3,18 @@
 # Posted by Alex  Granovsky, modified by community. See post 'Timeline' for change history
 # Retrieved 2026-06-16, License - CC BY-SA 4.0
 
-echo -n "stack[0][0] "
-python -m timeit -s 'import inspect, sys' 'inspect.stack()[0][0].f_code.co_name'
+#see stack.py for implementation
+# 1k iterations, setup 10 times. Echo and opts - RN
+opts="--number 1000 --setup 10"
+echo -n "stack[0][0].f_code "
+python -m timeit ${opts} -s 'import inspect, sys' 'inspect.stack()[0][0].f_code.co_name'
 
 echo -n "stack[0][3] "
-python -m timeit -s 'import inspect, sys' 'inspect.stack()[0][3]'
+python -m timeit ${opts} -s 'import inspect, sys' 'inspect.stack()[0][3]'
 
-echo -n "currentframe "
-python -m timeit -s 'import inspect, sys' 'inspect.currentframe().f_code.co_name'
+echo -n "currentframe.f_code "
+python -m timeit ${opts} -s 'import inspect, sys' 'inspect.currentframe().f_code.co_name'
 
-echo -n "sys "
-python -m timeit -s 'import inspect, sys' 'sys._getframe().f_code.co_name'
+echo -n "sys._getframe(3) "
+python -m timeit ${opts} -s 'import inspect, sys' 'sys._getframe(3).f_code.co_name'
 
